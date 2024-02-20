1bac30ab5a5aab456a6d02c9b57850c1caa4afbf
Booradas <info.minionuz@gmail.com>
2024-02-20 21:08:13 +0400

    fixed errors

--- hw-3.js
+++ hw-3.js
@@ -19,16 +19,24 @@ alert(Number(a) + Number(b));
 //--------------------------------------------------task 5--------------------------------------------------
 let monthNumber = Number(prompt('Введите номер месяца'));
 switch (monthNumber) {
-    case 1 || 2 || 12 :
+    case 1:
+    case 2:
+    case 12 :
         console.log('Winter'); 
         break;
-    case 3 || 4 || 5 :
+    case 3:
+    case 4:
+    case 5:
         console.log('Spring');     
         break;
-    case 6 || 7 || 8 :
+    case 6:
+    case 7:
+    case 8:
         console.log('Summer');
         break;
-    case 9 || 10 || 11 :
+    case 9:
+    case 10:
+    case 11:
         console.log('Autumn');
         break;
     default:
